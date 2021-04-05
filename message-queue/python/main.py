bbb74413791e8f50d1ad270cafe18ec992d5b6bd
이원렬 <dnjsfuf1030@naver.com>
2021-04-05 16:07:31 +0900

    added default connection string by azurite

--- message-queue/python/main.py
+++ message-queue/python/main.py
@@ -1,12 +1,13 @@
 import os, uuid
 from azure.storage.queue import (
-        QueueClient,
-        BinaryBase64EncodePolicy,
-        BinaryBase64DecodePolicy
+    QueueClient,
+    BinaryBase64EncodePolicy,
+    BinaryBase64DecodePolicy
 )
 
-connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-
+#connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+# This connection string is Azurite default connection string
+connect_str = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
 # Create a unique name for the queue
 q_name = "queue-" + str(uuid.uuid4())
 
@@ -14,26 +15,26 @@ q_name = "queue-" + str(uuid.uuid4())
 # be used to create and manipulate the queue
 print("Creating queue: " + q_name)
 
+queue_client = QueueClient.from_connection_string(connect_str, q_name)
+queue_client.create_queue()
+
 for i in range(2):
-                
-        queue_client = QueueClient.from_connection_string(connect_str, q_name + str(i))
-        # base64_queue_client = QueueClient.from_connection_string(
-        #                             conn_str=connect_str, queue_name=q_name,
-        #                             message_encode_policy = BinaryBase64EncodePolicy(),
-        #                             message_decode_policy = BinaryBase64DecodePolicy()
-        #                         )
+    # base64_queue_client = QueueClient.from_connection_string(
+    #                             conn_str=connect_str, queue_name=q_name,
+    #                             message_encode_policy = BinaryBase64EncodePolicy(),
+    #                             message_decode_policy = BinaryBase64DecodePolicy()
+    #                         )
 
-        message_str = "this is message" + str(i)
-        # Create the queue
-        queue_client.create_queue()
-        queue_client.send_message(message_str)
+    message_str = "this is message"
+    # Create the queue
+    queue_client.send_message(message_str + str(i))
 
-# Peek at the first message
 messages = queue_client.receive_messages(visibility_timeout=5*60)
-message = messages.by_page()
 
-for msg_batch in message:
-        for msg in msg_batch:
-                print("Dequeueing message: " + msg.content)
-                queue_client.delete_message(msg.id, msg.pop_receipt)
-                print("Deleted message: " + msg.content)
+for message in messages:
+    print("Dequeueing message: " + message.content)
+    queue_client.delete_message(message.id, message.pop_receipt)
+    print("Deleted message: " + message.content)
+            
+print("Deleting queue: " + queue_client.queue_name)
+queue_client.delete_queue()
